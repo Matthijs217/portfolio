c8cc57bd73e0a16e3b4e00358f4a5980a9656c88
Matthijs ten Brink <matthijstenbrink@gmail.com>
2025-10-01 19:52:40 +0200

    timeout weg #3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,72 +1,86 @@
 
-<h1 class="name">Matthijs</h1>
-<div id="set-height"></div>
-<video id="v0" tabindex="0" autobuffer="autobuffer" preload="preload" class="bg-video" muted>
-    <source type="video/mp4" src="/newyork4.mov">
-        macpro_main_desktop
-    </video>
-    
-    <style>
-        @font-face {
-            font-family: 'Gotham';
-            src: url('/fonts/GothamBold.otf') format('opentype');
-            font-weight: 700;
-            font-style: normal;
-        }
-        .name {
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -100%);
-            color: #F6FF00;
-            font-family: "Gotham", Arial, sans-serif;
-            font-size: 5rem;
-            font-weight: bold;
-            z-index: 10;
-        }
-        #set-height {
-            display: block;
-        }
-        .bg-video {
-
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100vw;
-            height: 100vh;
-            object-fit: cover;
-            z-index: -1;
-            /* pointer-events: none; */
-        } 
-    </style>
-    
-    <script>
-        import { onMount } from 'svelte';
+<script>
+    import { onMount } from 'svelte';
+    import { gsap } from 'gsap';
+    import { ScrollTrigger } from 'gsap/ScrollTrigger';
         
-        onMount(() => {
-            var frameNumber = 0, // start video at frame 0
-            // lower numbers = faster playback
-            playbackConst = 1000, 
-            // get page height from video duration
-            setHeight = document.getElementById("set-height"), 
-            // select video element         
-            vid = document.getElementById('v0'); 
-            
-            // dynamically set the page height according to video length
-            // vid.addEventListener('loadedmetadata', function() {
-                console.log("loadedmetadata")
-                setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
-                console.log(vid.duration);
-                console.log(setHeight.style.height);
-            // });
-            
-            // Use requestAnimationFrame for smooth playback
-            function scrollPlay() {  
-                var frameNumber  = window.pageYOffset/playbackConst;
-                vid.currentTime  = frameNumber;
-                window.requestAnimationFrame(scrollPlay);
-            }
-            
-            window.requestAnimationFrame(scrollPlay);
+    onMount(() => {
+        console.clear();
+
+        const video = document.querySelector(".video-background");
+        let src = video.currentSrc || video.src;
+        console.log(video, src);
+
+        function once(el, event, fn, opts) {
+            var onceFn = function (e) {
+                el.removeEventListener(event, onceFn);
+                fn.apply(this, arguments);
+            };
+            el.addEventListener(event, onceFn, opts);
+            return onceFn;
+        }
+
+        once(document.documentElement, "touchstart", function (e) {
+            video.play();
+            video.pause();
         });
-    </script>
+
+        gsap.registerPlugin(ScrollTrigger);
+
+        let tl = gsap.timeline({
+            defaults: { duration: 1 },
+            scrollTrigger: {
+                trigger: "#container",
+                start: "top top",
+                end: "bottom bottom",
+                scrub: true
+            }
+        });
+
+        once(video, "loadedmetadata", () => {
+            tl.fromTo(
+                video,
+                {
+                    currentTime: 0
+                },
+                {
+                    currentTime: video.duration || 1
+                }
+            );
+        });
+
+        if (window["fetch"]) {
+            fetch(src)
+                .then((response) => response.blob())
+                .then((response) => {
+                    var blobURL = URL.createObjectURL(response);
+
+                    var t = video.currentTime;
+                    once(document.documentElement, "touchstart", function (e) {
+                        video.play();
+                        video.pause();
+                    });
+
+                    video.setAttribute("src", blobURL);
+                    video.currentTime = t + 0.01;
+                });
+        }
+    });
+</script>
+<style>
+    .video-background {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        min-width: 100%;
+        min-height: 100%;
+        transform: translate(-50%, -50%);
+    }
+
+    #container {
+        height: 500vh;
+    }
+</style>
+<video src="newyork4_smooth.mp4" playsinline="true" webkit-playsinline="true" preload="auto" muted="muted" class="video-background"></video>
+
+<div id="container"></div>
