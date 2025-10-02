eaa1d89453a2854d8571c7232dd9cee411580cc4
Matthijs217 <matthijstenbrink@gmail.com>
2025-10-02 10:43:03 +0200

    prototype content #2
    Ik heb alvast wat content neer gezet om straks te kunnen gaan stylen.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,28 @@
 <video src="newyork4_smooth.mp4" playsinline="true" webkit-playsinline="true" preload="auto" muted="muted" class="video-background"></video>
 <h1 class="name">Matthijs</h1>
 <div id="container">
-    <div class="title">
-        <h2>About me</h2>
-    </div>
+    <main>
+        <div class="title">
+            <h2>About me</h2>
+        </div>
+        <div class="content">
+            <h3>AD Frontend Design & Development</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-    <h3>Education</h3>
-    <h3>Travel</h3>
-    <h3>Matthijs</h3>
-    <article>
-        <h4>Welcome to my website</h4>
-        <p>Scroll down to see the effect in action!</p>
-    </article>
+            <h3>America</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+            <h3>Hobbys</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#education">Education</a></li>
+                <li><a href="#travel">Travel</a></li>
+                <li><a href="#matthijs">Matthijs</a></li>
+            </ul>
+        </nav>
+    </main>
 </div>
 <style>
     @font-face {
@@ -23,6 +34,21 @@
         font-style: normal;
         font-display: swap;
     }  
+    #container {
+        height: 500vh;
+    }
+    main {
+        height:70vh;
+        width: 100%;
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        grid-template-rows: 1fr 2fr;
+        grid-template-areas:
+            "title title"
+            "nav nav"
+            "content content"
+        ;
+    }
     .video-background {
         position: fixed;
         top: 50%;
@@ -47,7 +73,6 @@
         animation-timeline: scroll(root);
         animation-range: 200px 600px;
     }
-    
     @keyframes fadeOutOnScroll {
         0% {
             opacity: 1;
@@ -58,9 +83,55 @@
             transform: translate(-50%, -150%);
         }
     }
-    #container {
-        height: 500vh;
+    .title {
+        grid-area: title;
+        display: flex;
+        justify-content: left;
+        align-items: center;
+        padding-left: 1rem;
+        h2 {
+            margin: 0;
+            font-family: 'Gotham', 'Arial', sans-serif;
+            font-size: 3rem;
+            color: #F6FF00;
+        }
     }
+    .content {
+        position: relative;
+        margin-left: 1rem;
+        padding: 1em;
+        width: 80vw;
+        color: rgb(0, 0, 0);
+        font-family: 'Gotham', 'Arial', sans-serif;
+        background-color: rgb(255, 255, 255);
+        grid-area: content;
+    }
+    nav {
+        grid-area: nav;
+        display: flex;
+        flex-direction: column;
+        padding-left: 1rem;
+        ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            li {
+                a {
+                    text-decoration: none;
+                    color: #F6FF00;
+                    font-family: 'Gotham', 'Arial', sans-serif;
+                    font-size: 1.5rem;
+                    &:hover {
+                        text-decoration: underline;
+                    }
+                }
+            }
+        }
+    }
+    
 </style>
 <script>
     import { onMount } from 'svelte';
