4b16ce6f3ece4948f8df3b1183cc085af2294c6b
Matthijs217 <matthijstenbrink@gmail.com>
2025-11-13 18:52:56 +0100

    Setup astro x sanity #1

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: 'r3k6w9b9',
+      dataset: 'production',
+      useCdn: false, // See note on using the CDN
+      apiVersion: "2025-11-13", // insert the current date to access the latest version of the API
+      studioBasePath: '/studio' // If you want to access the Studio on a route
+    }),
+    react(),
+  ],
+});
