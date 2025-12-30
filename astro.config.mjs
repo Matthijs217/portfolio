a1e500e9f913af2ba10fd68878fa691f9e245c18
Matthijs217 <matthijstenbrink@gmail.com>
2025-12-30 20:23:46 +0100

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   integrations: [
     sanity({
       projectId: 'r3k6w9b9',
